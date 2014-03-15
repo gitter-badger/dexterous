1bdb0fdf60e5d0a76178332d596b29022f66005e
lorefnon <lorefnon@gmail.com>
2014-03-15 22:41:34 +0530

    Incorporate associated models in enrollments endpoint response

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -2,7 +2,12 @@ class EnrollmentsController < ApplicationController
     before_filter :authenticate_user!
 
     def index
-        render json: current_user.enrollments
-            .includes(:achievements, track: [:milestones])
+        render json: current_user.enrollments.to_json(
+            include: [
+                :achievements, {
+                    track: {
+                        include: [:milestones]
+                    }
+                }])
     end
 end
