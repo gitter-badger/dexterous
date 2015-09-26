b3cd999977a7785a28fd2e967204bbc558c26051
lorefnon <lorefnon@gmail.com>
2015-09-27 01:46:20 +0530

    Cleanup

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,44 +1,45 @@
 class TracksController < ResourceController
 
-    layout 'dashboard'
+  layout 'dashboard'
 
-    skip_before_action :authenticate_user!, only: [:show]
-    skip_before_action :load_resource, only: [:show]
+  skip_before_action :authenticate_user!, only: [:show]
+  skip_before_action :load_resource, only: [:show]
 
-    def new
-        @model ||= Track.new
-    end
+  def new
+    @model ||= Track.new
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def create
-        @model = Track.new extract_params
-        @model.owner = current_user
-        @model.save!
-        redirect_to controller: 'home', action: 'dashboard'
-    rescue
-        render action: 'new'
-    end
+  def create
+    @model = Track.new extract_params
+    @model.owner = current_user
+    @model.save!
+    redirect_to controller: 'home', action: 'dashboard'
+  rescue
+    render action: 'new'
+  end
 
-    def show
-        @model = Track
-            .includes(
-                :learning_resources,
-                :milestones => [:achievements]
-             ).find params[:id]
-        head :not_found unless @model.viewable_by? current_user
-    end
+  def show
+    @model = Track
+      .includes(
+        :learning_resources,
+        :milestones => [:achievements]
+       ).find params[:id]
+    head :not_found unless @model.viewable_by? current_user
+  end
 
-    def timeline
-        render layout: 'application'
-    end
+  def timeline
+    render layout: 'application'
+  end
 
-    private
+  private
 
-    def extract_params
-        params
-            .require(:track)
-            .permit(:title, :description, :visibility, :contributability)
-    end
+  def extract_params
+    params
+      .require(:track)
+      .permit(:title, :description, :visibility, :contributability)
+  end
+
 end
