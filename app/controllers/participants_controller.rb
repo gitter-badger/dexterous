af37b708fd33b27dc78c8fe0d591ba8278ca08f9
lorefnon <lorefnon@gmail.com>
2015-10-01 00:42:27 +0530

    Add additional views

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,14 @@
 class ParticipantsController < ApplicationController
+
+  attr_accessor :track
+  helper_method :track
+  before_action :load_track
+  layout 'dashboard'
+
+  private
+
+  def load_track
+    @track = Track.friendly.find params[:track_id]
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,24 @@
 module ApplicationHelper
-    def resource_name
-        :user
+
+
+  def resource_name
+    :user
+  end
+  
+  def resource
+    @resource ||= User.new
+  end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def home_path
+    if user_signed_in? 
+      dashboard_path
+    else
+      root_path
     end
- 
-    def resource
-        @resource ||= User.new
-    end
- 
-    def devise_mapping
-        @devise_mapping ||= Devise.mappings[:user]
-    end
+  end
+
 end
